589e5d7fa3dd8f727d34382c0c26e848b8e1a831
Kemogne Alex <kemognemalone@gmail.com>
2025-10-04 18:37:06 +0200

    implemantation du header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className='home-wrapper flex min-h-screen'>Accueil</div>;
+};
+
+export default Home;
---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const NAV_ITEMS = [
+  { href: '/', label: 'Tableau de bord' },
+  { href: '/search', label: 'Recherche' },
+  { href: '/watchlist', label: 'Liste de suivi' },
+];
