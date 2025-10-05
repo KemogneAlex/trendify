1f73799d7e23b3beee9a145941a3e6b8d7a28807
Kemogne Alex <kemognemalone@gmail.com>
2025-10-06 00:36:46 +0200

    Correction de la redirection après inscription

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['flagcdn.com', 'flags.3-0-js.pages.dev'],
+  },
 };
 
 export default nextConfig;
